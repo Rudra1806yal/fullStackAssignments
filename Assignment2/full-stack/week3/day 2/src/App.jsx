32b13cc3dda4fecda7aa5b1bcaa814c17eb07b18
Rudra1806yal <rudrajuyal42@gmail.com>
2026-07-09 14:36:25 +0530

    Delete Assignment2/full-stack directory

--- Assignment2/full-stack/week3/day 2/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Components/Navbar"
-
-
-export default function App(){
-  return <div>
-    <h1>Hello from App Components</h1>
-    <Navbar/>
-  </div>
-}
--- Assignment2/full-stack/week3/day 2/src/Context/theme.jsx
+++
@@ -1,4 +0,0 @@
-import {createContext} from "react";
-
-//1. create a context
-export const theme = createContext();
